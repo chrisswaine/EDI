6977e29c0dc748c6fb4bec893cde3e4a104991fa
chris.swaine@causeway.com <chris@swaineview.co.uk>
2018-03-26 14:16:44 +0100

    play me some dirty synthwave

---
+++ commands/play.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+
+module.exports.run = async (bot, message, args) => {
+
+
+	if(message.content === 'some dirty synthwave') {
+		if (message.member.voiceChannel) {
+			const connection = await message.member.voiceChannel.join();
+			connection.play(ytdl(
+				'https://www.youtube.com/watch?v=0bwC1zJE49U&list=PLTgKUWDZ5rsz3sO77RiKSmrYr0aEBaXPQ',
+				{filter: 'audioonly'}));
+
+		} else {
+			message.reply('You need to join a voice channel first!');
+		}
+	}
+
+};
+
+module.exports.help = {
+	name: "play"
+};
